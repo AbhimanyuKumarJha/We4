41ba6cf63c467a98534d485d85074c406c840b6e
AbhimanyuKumarJha <jha.abhi1610@gmail.com>
2024-04-07 18:36:02 +0530

    meet update

--- src/components/Interview/meet.js
+++ src/components/Interview/meet.js
@@ -22,31 +22,54 @@ const InterviewMEET = () => {
 
   return (
     <>
-      <div className="w-full h-screen bg-slate-950 m-0 p-0 z-9">
-        <div
-          id="meet-navbar"
-          className="w-4/5 h-24 flex justify-between ml-10 items-center"
+      <div className="w-full h-screen bg-slate-950 m-0 p-0 z-9 flex items-center">
+        {/* EXIT */}
+        <button
+          onClick={toggleExitOption}
+          className="text-white text-lg absolute top-3 left-3"
         >
-          {/* EXIT */}
-          <button onClick={toggleExitOption} className="text-white text-lg">
-            <img src="" alt="Exit" />
-          </button>
-          <img src="" alt="LOGO" />
-        </div>
+          <img src="" alt="Exit" />
+        </button>
 
         {/* Main meet */}
-        <div className="main-meet flex h-4/5 m-auto items-center px-auto justify-around">
+        <div className="main-meet flex w-11/12 h-4/5 m-auto items-center px-auto justify-around">
           <div className="w-3/5 h-full rounded-lg bg-slate-500 "></div>
-          {/* time and command component */}
-          <div className="w-60 h-40 bg-slate-600 rounded-2xl">
-            <div className="w-full h-[70%] bg-red-400 rounded-t-2xl  ">
-              {QuestionTimer}
+          <div id="RIGHT-SIDE-OPTIONS" className="h-full flex items-center">
+            <div className="logo-meet absolute top-10 right-14 h-14 w-14 bg-slate-50">
+              <img src="" alt="LOGO" />{" "}
             </div>
-            <div className="flex w-full h-[30%] bg-slate-400 rounded-b-2xl"></div>
+
+            {/* time and command component */}
+            <div className="w-60 h-40 bg-slate-600 rounded-2xl">
+              <div className="w-full h-[70%] bg-red-400 rounded-t-2xl  ">
+                {QuestionTimer}
+              </div>
+              <div className="flex w-full h-[30%] justify-around rounded-b-2xl">
+                <div className="h-full w-[49%] bg-slate-400 rounded-bl-2xl ">
+                  Ask
+                </div>
+                <div className="rounded-br-2xl h-full w-[49%] bg-slate-400">
+                  Next
+                </div>
+              </div>
+            </div>
+
+            {/* OUR_CAMERA */}
           </div>
         </div>
       </div>
-      <div id="exits-options"></div>
+      <div
+        id="exits-options"
+        className="w-full h-full backdrop-blur-lg absolute top-0 bg-[rgba(23, 23, 23, 0.44)] "
+      >
+        <button
+          onClick={toggleExitOption}
+          className="text-white text-lg absolute top-3 left-3"
+        >
+          <img src="" alt="Exit" />
+        </button>
+        <div className=""></div>
+      </div>
     </>
   );
 };
