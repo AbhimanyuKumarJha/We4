06c12b55676e7b94bcaa647107e2905644420147
ayushanand <ayushanand106@gmail.com>
2024-04-08 19:09:58 +0530

    complete resume prediction model

--- Resume_prediction/app.py
+++ Resume_prediction/app.py
@@ -1,55 +1,53 @@
+#pip install -r requirement.txt
+# run the above command to install all required libraries
 import streamlit as st
-import google.generativeai as genai
-import os
 import PyPDF2 as pdf
-from dotenv import load_dotenv
 import json
+import os
+from dotenv import load_dotenv
+import google.generativeai as genai
 
-load_dotenv() ## load all our environment variables
+load_dotenv()  # load all our environment variables
 
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
-def get_gemini_repsonse(input):
-    model=genai.GenerativeModel('gemini-pro')
-    response=model.generate_content(input)
+
+def get_gemini_response(input_prompt):
+    model = genai.GenerativeModel('gemini-pro')
+    response = model.generate_content(input_prompt)
     return response.text
 
+
 def input_pdf_text(uploaded_file):
-    reader=pdf.PdfReader(uploaded_file)
-    text=""
+    reader = pdf.PdfReader(uploaded_file)
+    text = ""
     for page in range(len(reader.pages)):
-        page=reader.pages[page]
-        text+=str(page.extract_text())
+        page = reader.pages[page]
+        text += str(page.extract_text())
     return text
 
-#Prompt Template
 
-input_prompt="""
-Hey Act Like a skilled or very experience ATS(Application Tracking System)
-with a deep understanding of tech field,software engineering,data science ,data analyst
+# Prompt Template
+input_prompt = """
+Hey Act Like a skilled or very experienced ATS (Application Tracking System)
+with a deep understanding of the tech field, software engineering, data science, data analyst
 and big data engineer. Your task is to evaluate the resume based on the given job description.
 You must consider the job market is very competitive and you should provide 
-best assistance for improving thr resumes. Assign the percentage Matching based 
-on Jd and
-the missing keywords with high accuracy
+the best assistance for improving the resumes. Assign the percentage Matching based 
+on JD and the missing keywords with high accuracy
 resume:{text}
 description:{jd}
 
-I want the response in one single string having the structure
-Percentage Match
-MissingKeywords
-Profile Summary
+I want output in JSON format for example:
+
+Percentage Match: ["Percentage of resume match with JD"],
+MissingKeywords: ["Missing keywords from the resume that are required in JD"],
+Profile Summary: ["Your feedback on the resume of what it lacks"]
+
 """
-
-## streamlit app
-st.title("Resume Checker")
-jd=st.text_area("Paste the Job Description")
-uploaded_file=st.file_uploader("Upload Your Resume",type="pdf",help="Please uplaod the pdf")
-
-submit = st.button("Submit")
-
-if submit:
-    if uploaded_file is not None:
-        text=input_pdf_text(uploaded_file)
-        response=get_gemini_repsonse(input_prompt)
-        st.subheader(response)
+#function with input uploaded file(resume in pdf) and jd(in text)
+def get_response(uploaded_file,jd):
+    text=input_pdf_text(uploaded_file)
+    response = get_gemini_response(input_prompt.format(text=text, jd=jd))
+    json_data = json.loads(response)
+    return json_data
