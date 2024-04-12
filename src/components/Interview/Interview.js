2d5fe398977212a9722a5608760fb9dbdc5260f0
Hardik2972 <garghardik2972@gmail.com>
2024-04-12 15:48:17 +0530

    meet working

--- src/components/Interview/Interview.js
+++ src/components/Interview/Interview.js
@@ -4,16 +4,19 @@ import { useState } from "react";
 const Interview = () => {
   const [postID, setPostID] = useState("General");
   const [typeID, setTypeID] = useState("Behavioural");
-
+  const [roomId,setRoomId] = useState(null);
   const [selectedItem, setSelectedItem] = useState(null);
+
+  function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
 
   const handleItemClick = (item) => {
     setSelectedItem(item);
     console.log(selectedItem);
     const InterviewerLink = document.getElementById("copy-link");
     if (InterviewerLink) {
-      InterviewerLink.style.display =
-        selectedItem === "Online" ? "block" : "none";
+       setRoomId(getRandomNumber(1, 100));
     }
   };
   return (
@@ -58,13 +61,13 @@ const Interview = () => {
         <div className="h-screen w-full flex flex-col items-center justify-center relative snap-center">
           <div className="flex w-1/2 h-1/2 justify-around">
             <div
-              onClick={() => handleItemClick("bot1")}
+              onClick={() => handleItemClick("bot")}
               className="w-[10vw] h-[10vw] inline-block bg-slate-500 text-white cursor-pointer"
             >
               Bot1
             </div>
             <div
-              onClick={() => handleItemClick("bot2")}
+              onClick={() => handleItemClick("bot")}
               className="w-[10vw] h-[10vw] inline-block bg-slate-500 text-white cursor-pointer"
             >
               Bot2
@@ -76,7 +79,7 @@ const Interview = () => {
               AI4
             </div> */}
             <div
-              onClick={() => handleItemClick("Online")}
+              onClick={() => handleItemClick("online")}
               className={
                 " cursor-pointer w-[10vw] h-[10vw] inline-block text-white bg-slate-500"
               }
@@ -84,8 +87,9 @@ const Interview = () => {
               ONline
             </div>
           </div>
-          <div className=" w-1/4 h-[10%] rounded-sm bg-zinc-700" id="copy-link">
-            {"http://localhost:3000/Interviewer/"}
+          <div className=" w-1/4 h-[10%] rounded-sm bg-zinc-700 text-wrap text-white 
+          "  id="copy-link">
+            {"http://localhost:3000/Interview/Details/"+typeID+"/"+postID+"/ManualInterviewer/mylobby/online/interviewer/room/"+roomId}
           </div>
           <div className="absolute bottom-[5%] left-1/2 bg-amber-800 border-1 rounded-sm p-2">
             <Link
@@ -96,7 +100,8 @@ const Interview = () => {
                 postID +
                 "/" +
                 typeID +
-                "/ManualInterviewer/mylobby"
+                "/ManualInterviewer/mylobby/"+
+                selectedItem
               }
             >
               {" "}
