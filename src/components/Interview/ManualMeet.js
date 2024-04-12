d9eb006b8828fe6031d6591de00d1fbcb534139a
Hardik2972 <garghardik2972@gmail.com>
2024-04-12 10:20:19 +0530

    done

--- src/components/Interview/ManualMeet.js
+++ src/components/Interview/ManualMeet.js
@@ -48,7 +48,7 @@ const ManualInterviewMEET = (props) => {
       audio: true,
       video: true,
     });
-    console.log(stream.getVideoTracks());
+    console.log(stream);
     const offer = await peer.getOffer();
     socket.emit("user:call", { to: remoteSocketId, offer });
     setMyStream(stream);
